4fda175a2384c3edff697d1461d8701116da6ff0
collinewait <collinewait17@gmail.com>
2019-12-29 16:23:52 +0300

    download attachments

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -1,20 +1,73 @@
 package scraper
 
 import (
+	"archive/zip"
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 	"strings"
+	"sync"
+	"time"
+
+	"github.com/collinewait/ika-gmail-scraper/oauth"
+	"google.golang.org/api/gmail/v1"
 )
+
+const userID = "me"
+
+// create fileAddress based on current time
+var fileAddress string
 
 // Scrape will extract attachments contained in mails sent by a specific email.
 func Scrape(w http.ResponseWriter, r *http.Request) {
+	emailThatSentAttach := r.FormValue("emailThatSentAttach")
+	fileAddress = "attachments" + strconv.FormatInt(time.Now().Unix(), 10) + ".zip"
 	token, err := extractToken(r)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(token)) // nolint
+	service := oauth.GetGmailService(token)
+
+	var ms messageSevice
+	var cont content
+	var as attachmentService
+
+	ms = &message{}
+	cont = &messageContent{}
+	as = &attachment{}
+
+	doneChannel := make(chan bool)
+	messagesChannel, getIDsErr := getIDs(service, emailThatSentAttach, ms)
+	if len(getIDsErr) != 0 {
+		err := <-getIDsErr
+		msg := err.msg + " " + err.err.Error()
+		errorResponse(w, msg)
+		return //nolint
+	}
+	messageContentChannel, getMsgCErr := getMessageContent(messagesChannel, service, cont)
+	if len(getMsgCErr) != 0 {
+		err := <-getMsgCErr
+		msg := err.msg + " " + err.err.Error()
+		errorResponse(w, msg)
+	}
+	attachmentChannel, getAttachErr := getAttachment(messageContentChannel, service, as)
+	if len(getAttachErr) != 0 {
+		err := <-getAttachErr
+		msg := err.msg + " " + err.err.Error()
+		errorResponse(w, msg)
+	}
+
+	go saveAttachment(attachmentChannel, doneChannel)
+
+	<-doneChannel
+
+	w.Header().Set("Content-type", "application/zip")
+	http.ServeFile(w, r, fileAddress)
+
+	defer os.Remove(fileAddress)
 }
 
 func extractToken(r *http.Request) (string, error) {
@@ -26,4 +79,224 @@ func extractToken(r *http.Request) (string, error) {
 
 	reqToken = strings.TrimSpace(splitToken[1])
 	return reqToken, nil
+}
+
+func errorResponse(w http.ResponseWriter, message string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusBadRequest)
+	errMsg := fmt.Sprintf(`{"error": "%s"}`, message)
+
+	w.Write([]byte(errMsg)) // nolint
+}
+
+type messageSevice interface {
+	fetchMessages(service *gmail.Service,
+		query string) (*gmail.ListMessagesResponse, error)
+	fetchNextPage(service *gmail.Service,
+		query string,
+		NextPageToken string) (*gmail.ListMessagesResponse, error)
+}
+type content interface {
+	getContent(
+		service *gmail.Service, id string) (*gmail.Message, error)
+}
+type attachmentService interface {
+	fetchAttachment(
+		service *gmail.Service,
+		msgID string, attachID string) (*gmail.MessagePartBody, error)
+}
+
+type message struct{}
+type messageContent struct{}
+type messageError struct {
+	err error
+	msg string
+}
+type attachment struct {
+	data     string
+	fileName string
+}
+
+func (m *message) fetchMessages(
+	service *gmail.Service,
+	query string) (*gmail.ListMessagesResponse, error) {
+	r, err := service.Users.Messages.List(userID).Q(query).Do()
+	return r, err
+}
+
+func (m *message) fetchNextPage(
+	service *gmail.Service,
+	query string,
+	NextPageToken string) (*gmail.ListMessagesResponse, error) {
+	r, err := service.Users.Messages.List(userID).Q(query).
+		PageToken(NextPageToken).Do()
+	return r, err
+}
+
+func getIDs(service *gmail.Service,
+	email string,
+	m messageSevice) (<-chan string, <-chan *messageError) {
+	errs := new(messageError)
+	errorsCh := make(chan *messageError, 1)
+	defer close(errorsCh)
+	query := fmt.Sprintf("from:%s", email)
+
+	msgs := []*gmail.Message{}
+
+	r, err := m.fetchMessages(service, query)
+	if err != nil {
+		errs.msg = "Unable to retrieve Messages"
+		errs.err = err
+		errorsCh <- errs
+		return nil, errorsCh
+	}
+	msgs = append(msgs, r.Messages...)
+
+	for len(r.NextPageToken) != 0 {
+		r, err = m.fetchNextPage(service, query, r.NextPageToken)
+		if err != nil {
+			errs.msg = "Unable to retrieve Messages on the next page"
+			errs.err = err
+			errorsCh <- errs
+			return nil, errorsCh
+		}
+		msgs = append(msgs, r.Messages...)
+	}
+
+	if len(r.Messages) == 0 {
+		fmt.Println("No messages found.")
+	}
+
+	ids := make(chan string)
+
+	var wg sync.WaitGroup
+	for _, msg := range msgs {
+		wg.Add(1)
+		go func(msg *gmail.Message) {
+			defer wg.Done()
+			ids <- msg.Id
+		}(msg)
+	}
+
+	go func() {
+		wg.Wait()
+		close(ids)
+	}()
+
+	return ids, nil
+}
+
+func (mc *messageContent) getContent(
+	service *gmail.Service, id string) (*gmail.Message, error) {
+	return service.Users.Messages.Get(userID, id).Do()
+}
+
+func getMessageContent(
+	ids <-chan string,
+	service *gmail.Service,
+	c content) (<-chan *gmail.Message, <-chan *messageError) {
+	msgCh := make(chan *gmail.Message)
+	errs := new(messageError)
+	errorsCh := make(chan *messageError, 1)
+	var wg sync.WaitGroup
+	for id := range ids {
+		fmt.Println("Getting MessageContent....")
+		wg.Add(1)
+		go func(id string) {
+			defer wg.Done()
+			msgContent, err := c.getContent(service, id)
+			if err != nil {
+				errs.msg = "Unable to retrieve Message Contents"
+				errs.err = err
+				errorsCh <- errs
+				close(errorsCh)
+			}
+			msgCh <- msgContent
+		}(id)
+	}
+	go func() {
+		wg.Wait()
+		close(msgCh)
+	}()
+	return msgCh, errorsCh
+}
+
+func (a *attachment) fetchAttachment(
+	service *gmail.Service, msgID string, attachID string) (*gmail.MessagePartBody, error) {
+	return service.Users.Messages.Attachments.
+		Get(userID, msgID, attachID).Do()
+}
+
+func getAttachment(
+	msgContentCh <-chan *gmail.Message,
+	service *gmail.Service,
+	as attachmentService,
+) (<-chan *attachment, <-chan *messageError) {
+	var wg sync.WaitGroup
+	attachCh := make(chan *attachment)
+	errs := new(messageError)
+	errorsCh := make(chan *messageError, 1)
+
+	for msgContent := range msgContentCh {
+		fmt.Println("Getting attachment....")
+		wg.Add(1)
+		go func(msgContent *gmail.Message) {
+			defer wg.Done()
+			attach := new(attachment)
+			tm := time.Unix(0, msgContent.InternalDate*1e6)
+			for _, part := range msgContent.Payload.Parts {
+				if len(part.Filename) != 0 {
+					newFileName := tm.Format("Jan-02-2006") + "-" + part.Filename
+					msgPartBody, err := as.fetchAttachment(service, msgContent.Id, part.Body.AttachmentId)
+					if err != nil {
+						errs.msg = "Unable to retrieve Attachment"
+						errs.err = err
+						errorsCh <- errs
+						close(errorsCh)
+					}
+					attach.data = msgPartBody.Data
+					attach.fileName = newFileName
+					attachCh <- attach
+				}
+			}
+		}(msgContent)
+	}
+	go func() {
+		wg.Wait()
+		close(attachCh)
+	}()
+
+	return attachCh, errorsCh
+}
+
+func saveAttachment(attachCh <-chan *attachment, doneCh chan bool) {
+	outFile, err := os.Create(fileAddress)
+	if err != nil {
+		fmt.Println("Error on os.Create. ", err.Error())
+	}
+	defer outFile.Close()
+
+	zw := zip.NewWriter(outFile)
+	var wg sync.WaitGroup
+	for attach := range attachCh {
+		fmt.Println("Saving attachment....")
+		wg.Add(1)
+		go func(attach *attachment) {
+			defer wg.Done()
+			decoded, _ := base64.URLEncoding.DecodeString(attach.data)
+			f, err := zw.Create(attach.fileName)
+			if err != nil {
+				fmt.Println("Error on zw.Create. ", err.Error())
+			}
+			if _, err := f.Write(decoded); err != nil {
+				fmt.Println("Error on f.Write. ", err.Error())
+			}
+		}(attach)
+	}
+	wg.Wait()
+	err = zw.Close()
+	if err != nil {
+		fmt.Println("Error on zw.Close. ", err.Error())
+	}
+	doneCh <- true
 }
--- scraper/scraper_test.go
+++ scraper/scraper_test.go
@@ -1,9 +1,15 @@
 package scraper
 
 import (
+	"errors"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
+	"sort"
 	"testing"
+	"time"
+
+	"google.golang.org/api/gmail/v1"
 )
 
 func Test_extractToken_shouldReturnToken(t *testing.T) {
@@ -31,5 +37,415 @@ func Test_extractToken_shouldReturnError(t *testing.T) {
 	_, err := extractToken(r)
 	if err.Error() != expectedError {
 		t.Errorf("extractToken() = %v, want %v", err.Error(), expectedError)
+	}
+}
+
+type mockMessageSevice interface {
+	fetchMessages(
+		service *gmail.Service,
+		query string) (*gmail.ListMessagesResponse, error)
+	fetchNextPage(service *gmail.Service,
+		query string,
+		NextPageToken string) (*gmail.ListMessagesResponse, error)
+}
+
+type mockMessage struct {
+}
+
+func (m *mockMessage) fetchMessages(
+	service *gmail.Service,
+	query string) (*gmail.ListMessagesResponse, error) {
+	gm := []*gmail.Message{
+		{Id: "16c2"},
+		{Id: "41ff9"},
+		{Id: "41hfi"},
+		{Id: "fgb"},
+		{Id: "ifgh9"},
+	}
+
+	r := gmail.ListMessagesResponse{
+		Messages: gm,
+	}
+	return &r, nil
+}
+func (m *mockMessage) fetchNextPage(
+	service *gmail.Service,
+	query string,
+	NextPageToken string) (*gmail.ListMessagesResponse, error) {
+	r := gmail.ListMessagesResponse{
+		Messages: []*gmail.Message{},
+	}
+	return &r, nil
+}
+
+func Test_getIDsCanReturnIDsWithoutNextPageToken(t *testing.T) {
+	tests := []struct {
+		name string
+		want []string
+	}{
+		{
+			name: "test case without NextPageToken",
+			want: []string{"16c2", "41ff9", "41hfi", "fgb", "ifgh9"},
+		},
+	}
+	service := new(gmail.Service)
+	testmail := "test@mail.com"
+	var ms mockMessageSevice = &mockMessage{}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cgot, _ := getIDs(service, testmail, ms)
+			var got []string
+
+			for i := range cgot {
+				got = append(got, i)
+			}
+
+			sort.Strings(sort.StringSlice(got))
+			sort.Strings(sort.StringSlice(tt.want))
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getIDs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+type mockMessageWithNextPage struct {
+}
+
+func (m *mockMessageWithNextPage) fetchMessages(
+	service *gmail.Service,
+	query string) (*gmail.ListMessagesResponse, error) {
+	gm := []*gmail.Message{
+		{Id: "16c2"},
+		{Id: "41ff9"},
+		{Id: "41hfi"},
+		{Id: "fgb"},
+		{Id: "ifgh9"},
+	}
+
+	r := gmail.ListMessagesResponse{
+		Messages:      gm,
+		NextPageToken: "someToken",
+	}
+	return &r, nil
+}
+
+func (m *mockMessageWithNextPage) fetchNextPage(
+	service *gmail.Service,
+	query string,
+	NextPageToken string) (*gmail.ListMessagesResponse, error) {
+	gm := []*gmail.Message{
+		{Id: "fgbmm"},
+	}
+
+	r := gmail.ListMessagesResponse{
+		Messages: gm,
+	}
+	return &r, nil
+}
+
+func Test_getIDsCanReturnIDsWithNextPageToken(t *testing.T) {
+	tests := []struct {
+		name string
+		want []string
+	}{
+		{
+			name: "test case with NextPageToken",
+			want: []string{"16c2", "41ff9", "41hfi", "fgb", "ifgh9", "fgbmm"},
+		},
+	}
+	service := new(gmail.Service)
+	testmail := "test@mail.com"
+	var ms mockMessageSevice = &mockMessageWithNextPage{}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cgot, _ := getIDs(service, testmail, ms)
+			var got []string
+
+			for i := range cgot {
+				got = append(got, i)
+			}
+
+			sort.Strings(sort.StringSlice(got))
+			sort.Strings(sort.StringSlice(tt.want))
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getIDs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+type mockMessageWithFetchMessagesError struct {
+}
+
+func (m *mockMessageWithFetchMessagesError) fetchMessages(
+	service *gmail.Service,
+	query string) (*gmail.ListMessagesResponse, error) {
+	r := gmail.ListMessagesResponse{
+		Messages: []*gmail.Message{},
+	}
+	return &r, errors.New("Couldn't fetch messages")
+}
+
+func (m *mockMessageWithFetchMessagesError) fetchNextPage(
+	service *gmail.Service,
+	query string,
+	NextPageToken string) (*gmail.ListMessagesResponse, error) {
+
+	r := gmail.ListMessagesResponse{
+		Messages: []*gmail.Message{},
+	}
+	return &r, nil
+}
+
+func Test_getIDsShouldReturnErrorsReturnedByFetchMessages(t *testing.T) {
+	service := new(gmail.Service)
+	testmail := "test@mail.com"
+	var ms mockMessageSevice = &mockMessageWithFetchMessagesError{}
+
+	_, err := getIDs(service, testmail, ms)
+	expected := "Unable to retrieve Messages"
+	for e := range err {
+		if e.msg != expected {
+			t.Errorf("getIDs() = %v, want %v", e.msg, expected)
+		}
+	}
+
+}
+
+type mockMessageWithFetchNextPageError struct {
+}
+
+func (m *mockMessageWithFetchNextPageError) fetchMessages(
+	service *gmail.Service,
+	query string) (*gmail.ListMessagesResponse, error) {
+	r := gmail.ListMessagesResponse{
+		Messages:      []*gmail.Message{{Id: "16c2"}},
+		NextPageToken: "someTokenHere",
+	}
+	return &r, nil
+}
+
+func (m *mockMessageWithFetchNextPageError) fetchNextPage(
+	service *gmail.Service,
+	query string,
+	NextPageToken string) (*gmail.ListMessagesResponse, error) {
+
+	r := gmail.ListMessagesResponse{
+		Messages: []*gmail.Message{},
+	}
+	return &r, errors.New("Couldn't fetch messages on next page")
+}
+
+func Test_getIDsShouldReturnErrorsReturnedByFetchNextPage(t *testing.T) {
+	service := new(gmail.Service)
+	testmail := "test@mail.com"
+	var ms mockMessageSevice = &mockMessageWithFetchNextPageError{}
+
+	_, err := getIDs(service, testmail, ms)
+	expected := "Unable to retrieve Messages on the next page"
+	for e := range err {
+		if e.msg != expected {
+			t.Errorf("getIDs() = %v, want %v", e.msg, expected)
+		}
+	}
+
+}
+
+type mockMessageWithoutMessages struct {
+}
+
+func (m *mockMessageWithoutMessages) fetchMessages(
+	service *gmail.Service,
+	query string) (*gmail.ListMessagesResponse, error) {
+	r := gmail.ListMessagesResponse{
+		Messages: []*gmail.Message{},
+	}
+	return &r, nil
+}
+
+func (m *mockMessageWithoutMessages) fetchNextPage(
+	service *gmail.Service,
+	query string,
+	NextPageToken string) (*gmail.ListMessagesResponse, error) {
+
+	r := gmail.ListMessagesResponse{
+		Messages: []*gmail.Message{},
+	}
+	return &r, nil
+}
+
+func Test_getIDsWithoutMessages(t *testing.T) {
+	service := new(gmail.Service)
+	testmail := "test@mail.com"
+	var ms mockMessageSevice = &mockMessageWithoutMessages{}
+
+	msgs, _ := getIDs(service, testmail, ms)
+	if len(msgs) != 0 {
+		t.Errorf("getIDs() = %v, want %v", len(msgs), 0)
+	}
+}
+
+type mockMessageContent struct {
+}
+
+type mockContent interface {
+	getContent(
+		service *gmail.Service, id string) (*gmail.Message, error)
+}
+
+func generateIds() <-chan string {
+	idsCh := make(chan string, 1)
+	ids := []string{"someId"}
+	defer close(idsCh)
+	for _, v := range ids {
+		idsCh <- v
+	}
+
+	return idsCh
+}
+
+func (m *mockMessageContent) getContent(
+	service *gmail.Service, id string) (*gmail.Message, error) {
+	gm := gmail.Message{
+		Payload: &gmail.MessagePart{
+			Filename: "somename.pdf",
+		},
+	}
+	return &gm, nil
+}
+
+func Test_getMessageContent(t *testing.T) {
+	service := new(gmail.Service)
+	var mc mockContent = &mockMessageContent{}
+	msgCh := generateIds()
+	filename := "somename.pdf"
+
+	msgs, _ := getMessageContent(msgCh, service, mc)
+
+	for m := range msgs {
+		if m.Payload.Filename != filename {
+			t.Errorf("getMessageContent() = %v, want %v", m.Payload.Filename, filename)
+		}
+	}
+}
+
+type mockMessageContentWithGetContentError struct {
+}
+
+func (m *mockMessageContentWithGetContentError) getContent(
+	service *gmail.Service, id string) (*gmail.Message, error) {
+	gm := gmail.Message{
+		Payload: &gmail.MessagePart{
+			Filename: "",
+		},
+	}
+	return &gm, errors.New("Error when getting contents")
+}
+
+func Test_getMessageContentWithGetContentError(t *testing.T) {
+	service := new(gmail.Service)
+	var mc mockContent = &mockMessageContentWithGetContentError{}
+	msgCh := generateIds()
+
+	_, err := getMessageContent(msgCh, service, mc)
+
+	expected := "Unable to retrieve Message Contents"
+	for e := range err {
+		if e.msg != expected {
+			t.Errorf("getMessageContent() = %v, want %v", e.msg, expected)
+		}
+	}
+}
+
+type mockAttachment struct {
+}
+
+type mockAttachmentService interface {
+	fetchAttachment(
+		service *gmail.Service,
+		msgID string, attachID string) (*gmail.MessagePartBody, error)
+}
+
+func generateMsgsContents() <-chan *gmail.Message {
+	msgsCh := make(chan *gmail.Message, 1)
+	layout := "01/02/2006 3:04:05 PM"
+	t, _ := time.Parse(layout, "11/20/2019 2:03:46 PM")
+	msgs := []*gmail.Message{
+		{
+			InternalDate: t.UnixNano(),
+			Payload: &gmail.MessagePart{
+				Parts: []*gmail.MessagePart{
+					{
+						Filename: "attachmentfile.pdf",
+						Body: &gmail.MessagePartBody{
+							AttachmentId: "attachmentId",
+						},
+					},
+				},
+			},
+			Id: "msgIdhere",
+		},
+	}
+	defer close(msgsCh)
+	for _, v := range msgs {
+		msgsCh <- v
+	}
+
+	return msgsCh
+}
+
+func (a *mockAttachment) fetchAttachment(
+	service *gmail.Service,
+	msgID string, attachID string) (*gmail.MessagePartBody, error) {
+	attachment := gmail.MessagePartBody{
+		Data: "some attachment Data Here",
+	}
+
+	return &attachment, nil
+}
+
+func Test_getAttachment(t *testing.T) {
+	service := new(gmail.Service)
+	var as mockAttachmentService = &mockAttachment{}
+	msgContents := generateMsgsContents()
+	data := "some attachment Data Here"
+
+	atts, _ := getAttachment(msgContents, service, as)
+
+	for a := range atts {
+		if a.data != data {
+			t.Errorf("getAttachment() = %v, want %v", a.data, data)
+		}
+	}
+}
+
+type mockAttachmentWithFetchError struct {
+}
+
+func (a *mockAttachmentWithFetchError) fetchAttachment(
+	service *gmail.Service,
+	msgID string, attachID string) (*gmail.MessagePartBody, error) {
+	attachment := gmail.MessagePartBody{}
+
+	return &attachment, errors.New("Error fetching attachment")
+}
+
+func Test_getAttachmentWithFetchError(t *testing.T) {
+	service := new(gmail.Service)
+	var as mockAttachmentService = &mockAttachmentWithFetchError{}
+	msgContents := generateMsgsContents()
+
+	_, err := getAttachment(msgContents, service, as)
+
+	expected := "Unable to retrieve Attachment"
+	for e := range err {
+		if e.msg != expected {
+			t.Errorf("getMessageContent() = %v, want %v", e.msg, expected)
+		}
 	}
 }
